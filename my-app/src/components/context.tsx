a24fdee615632b9bf3099ae4a7411e62c02a5823
KimEunA97 <kimeuna9712@gmail.com>
2023-07-08 00:22:30 +0900

    객체배열 데이터 넘기긱

---
+++ my-app/src/components/context.tsx
@@ -0,0 +1,55 @@
+import { createContext, useContext } from 'react';
+const MyContext = createContext<MyContextType>({});
+
+
+interface MyContextType {
+  [key: string]: string[];
+}
+
+const arr = {
+
+  a: ['a'],
+  b: ['b'],
+  c: ['c'],
+
+}
+
+function App() {
+  return (
+    <MyContext.Provider value={arr}>
+      <GrandParent />
+    </MyContext.Provider>
+  );
+}
+
+function GrandParent() {
+  return <Parent />;
+}
+
+function Parent() {
+  return <Child />;
+}
+
+function Child() {
+  return <GrandChild />;
+}
+
+function GrandChild() {
+  return <Message />;
+}
+
+function Message() {
+  const value = useContext(MyContext);
+  return (
+    <div>
+      Received:
+      {Object.entries(value).map(([key, arr]) => (
+        <div key={key}>
+          {key}: {arr.join(', ')}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
